8b74f2b9b8a50677f0eb24dd262a6b2e4545515f
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-28 13:45:05 +0530

    login

---
+++ src/app/employeeentry/employeeentry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employeeentry',
+  templateUrl: './employeeentry.component.html',
+  styleUrls: ['./employeeentry.component.css']
+})
+export class EmployeeentryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
