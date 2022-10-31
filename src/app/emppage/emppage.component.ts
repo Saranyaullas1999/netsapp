da6124bd0950acf5a351c9ac1160e282305eb5d8
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-31 21:38:07 +0530

    employee login

---
+++ src/app/emppage/emppage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emppage',
+  templateUrl: './emppage.component.html',
+  styleUrls: ['./emppage.component.css']
+})
+export class EmppageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
